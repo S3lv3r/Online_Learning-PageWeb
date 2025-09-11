6d8568b9551131787bd4ebf9e931581f39f09eb9
S3lv3r <fernanfc3098@gmail.com>
2025-09-11 14:09:24 -0600

    feat(header-up) agregar header nav

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,14 @@
+import logo from "../assets/logo_skilline_bg.svg"
+import "../components/header-style.css"
 function Header() {
     return (
         <>
-            <div>
+            <div className="background-header">
                 <div className="header-up">
                     <div className="logo-header-up">
-                        
+                        <img src={logo} alt="Logo" />
                     </div>
+                    <div className="items-upper-header-up"></div>
                     <div className="items-header-up">
                         <nav>
                             <ul>
@@ -18,9 +21,27 @@ function Header() {
                         </nav>
                     </div>
                     <div className="account-header-up">
-                        <button>Login</button>
-                        <button>Sign Up</button>
+                        <button className="-button-login">Login</button>
+                        <button className="-button-signup">Sign Up</button>
                     </div>
+                </div>
+                <div className="header-down">
+                    <div className="header-down-leftSide">
+                        <div className="-leftSide-statement">
+                            <p>
+                                <strong>Studying</strong> Online is now much easier
+                            </p>
+                        </div>
+                        <div className="-leftSide-resume">
+                            <p>Skilline is an interesting platform that will teach you in more an interactive way</p>
+                        </div>
+                        <div className="-leftSide-buttons">
+                            <button>Join for free</button>
+                            <button>Play (Is a icon)</button>
+                            <p>Watch how it works</p>
+                        </div>
+                    </div>
+                    <div className="header-down-rightSide"></div>
                 </div>
             </div>
         </>
