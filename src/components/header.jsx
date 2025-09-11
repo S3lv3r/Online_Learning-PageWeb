51caecc54ab8e78bf9440c91150dc50e41a33698
S3lv3r <fernanfc3098@gmail.com>
2025-09-10 23:22:40 -0600

    Initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,30 @@
+function Header() {
+    return (
+        <>
+            <div>
+                <div className="header-up">
+                    <div className="logo-header-up">
+                        
+                    </div>
+                    <div className="items-header-up">
+                        <nav>
+                            <ul>
+                                <a href="http://">Home</a>
+                                <a href="http://">Careers</a>
+                                <a href="http://">Blog</a>
+                                <a href="http://">About Us</a>
+
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="account-header-up">
+                        <button>Login</button>
+                        <button>Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Header
