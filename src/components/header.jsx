6f7bf7dcdb84ec5be4aaa768f7ee27689ca898ee
S3lv3r <fernanfc3098@gmail.com>
2025-09-14 12:19:19 -0600

    feat(header) header terminado

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,9 @@
 import logo from "../assets/logo_skilline_bg.svg"
+import girl from "../assets/girl-header.svg"
+import profile from "../assets/profile.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarDays } from '@fortawesome/free-regular-svg-icons'
+import { faPlay,faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import "../components/header-style.css"
 function Header() {
     return (
@@ -36,12 +41,44 @@ function Header() {
                             <p>Skilline is an interesting platform that will teach you in more an interactive way</p>
                         </div>
                         <div className="-leftSide-buttons">
-                            <button>Join for free</button>
-                            <button>Play (Is a icon)</button>
+                            <button className="-leftSide-buttons-joinBTN">Join for free</button>
+                            <button className="-leftSide-buttons-playBTN"><FontAwesomeIcon icon={faPlay} className="-playBTN-icon"/></button>
                             <p>Watch how it works</p>
                         </div>
                     </div>
-                    <div className="header-down-rightSide"></div>
+                    <div className="header-down-rightSide">
+                        <img src={girl} className="-rightSide-background-img" />
+                        <div className="-rightSide-background-card" id="assistedDiv">
+                            <div className="rightSide-background-card-img">
+                                <div><FontAwesomeIcon icon={faCalendarDays} className="-card-img-fontawesome"/></div>
+                                
+                            </div>
+                            <div className="rightSide-background-card-text">
+                                <span>250K</span>
+                                <p>Assisted Student</p>
+                            </div>
+                        </div>
+                        <div className="-rightSide-background-card" id="congratulationsDiv">
+                            <div className="rightSide-background-card-img">
+                                <div><FontAwesomeIcon icon={faEnvelope} className="-card-img-fontawesome"/></div>
+                                
+                            </div>
+                            <div className="rightSide-background-card-text">
+                                <span>Congratulations</span>
+                                <p>Your admissions completed</p>
+                            </div>
+                        </div>
+                        <div className="-rightSide-background-card" id="userDiv">
+                            <div className="rightSide-background-card-img">
+                                <img src={profile} alt="userProfile" />
+                            </div>
+                            <div className="rightSide-background-card-text">
+                                <span>User Experience Class</span>
+                                <p>Today at 12.00 PM</p>
+                                <button>Join Now</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
