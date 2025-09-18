c0f831fee99f106e719daeb8712987e6e03f6e3c
S3lv3r <fernanfc3098@gmail.com>
2025-09-17 21:38:56 -0600

    fix(button styled): se cambio la manera en que pasa variables, ahora yo no es directo, sino mediante variables de css

--- src/components/ui/button-styled.jsx
+++ src/components/ui/button-styled.jsx
@@ -1,8 +1,8 @@
 import styles from "./button-styled.module.css"
 
-export default function ButtonStyled({border,backgroundColor,textColor, text}){
+export default function ButtonStyled({border,backgroundColor,textColor, text}){ // El uso de variables en CSS, hace que se afecte y no se sobre ponga 
     return(
-        <button className={styles.btn} style={{ backgroundColor: backgroundColor, color: textColor, border: border }}>
+        <button className={styles.btn} style={{ "--btn-bg": backgroundColor, "--btn-color": textColor, "--btn-border": border }}> 
             {text}
         </button>
     );
